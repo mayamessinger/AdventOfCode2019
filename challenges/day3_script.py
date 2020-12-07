ffe8ca2e3bc1edaa9cb6f5f43cff5443911a988a
Maya Messinger <maya.messinger@gmail.com>
2020-12-06 23:58:03 -0600

    day 3 - working part 2

--- challenges/day3_script.py
+++ challenges/day3_script.py
@@ -1,14 +1,19 @@
 import re
 
 # start and end points are "oriented" such that start coordiate will always be
-# smaller than end coordinate (smaller == closer to (0, 0))
+# smaller than end coordinate (smaller == lower/further left)
 class Path:
 	def __init__(self, x1, y1, x2, y2):
-		self.start = Coordinate(min(x1, x2), min(y1, y2))
-		self.end = Coordinate(max(x1, x2), max(y1, y2))
+		self.start = Coordinate(x1, y1)
+		self.end = Coordinate(x2, y2)
+		self.low = Coordinate(min(x1, x2), min(y1, y2))
+		self.high = Coordinate(max(x1, x2), max(y1, y2))
 
 	def __eq__(self, obj):
-		return isinstance(obj, Path) and self.start == obj.start and self.end == obj.end
+		return isinstance(obj, Path) and self.low == obj.low and self.high == obj.high
+
+	def distance(self):
+		return max(abs(self.high.x - self.low.x), abs(self.high.y - self.low.y))
 
 # all x and y values are relative to the central port (0, 0)
 class Coordinate:
@@ -22,6 +27,11 @@ class Coordinate:
 	def distanceFromCenter(self):
 		return abs(self.x) + abs(self.y)
 
+class Intersection:
+	def __init__(self, coordinate, pathLengths):
+		self.coordinate = coordinate
+		self.steps = pathLengths
+
 def run(inputFile):
 	contents = open(inputFile, "r")
 	wireMoves = contents.read().split("\n")
@@ -30,7 +40,7 @@ def run(inputFile):
 	allPaths = mapAllWires(wireMoves)
 	intersections = getIntersections(allPaths)
 
-	print(getClosestIntersection(intersections).distanceFromCenter())
+	print(getClosestIntersection(intersections).steps)
 
 # returns an array of arrays, each subarray represents one wire's paths
 # paths are objects with absolute start and end points
@@ -77,27 +87,38 @@ def getIntersections(paths):
 	wireA = paths[0]
 	wireB = paths[1]
 
+	aSteps = 0
+	bSteps = 0
 	for a in range(0, len(wireA)):
 		for b in range(0, len(wireB)):
 			cross = crosses(wireA[a], wireB[b])
 			if (cross is not None):
-				intersections.append(cross)
+				intersections.append(Intersection(cross, aSteps + bSteps + intersectionDistance(cross, wireA[a], wireB[b])))
+
+			bSteps += wireB[b].distance()
+
+		aSteps += wireA[a].distance()
+		bSteps = 0
 
 	return intersections
 
 def crosses(pathA, pathB):
-	if (pathA.start.x <= pathB.start.x <= pathA.end.x
-		and pathB.start.y <= pathA.start.y <= pathB.end.y):
-			return Coordinate(pathB.start.x, pathA.start.y)
+	if (pathA.low.x <= pathB.low.x <= pathA.high.x
+		and pathB.low.y <= pathA.low.y <= pathB.high.y):
+			return Coordinate(pathB.low.x, pathA.low.y)
 
-	if (pathA.start.y <= pathB.start.y <= pathA.end.y
-		and pathB.start.x <= pathA.start.x <= pathB.end.x):
-			return Coordinate(pathA.start.x, pathB.start.y)
+	if (pathA.low.y <= pathB.low.y <= pathA.high.y
+		and pathB.low.x <= pathA.low.x <= pathB.high.x):
+			return Coordinate(pathA.low.x, pathB.low.y)
+
+def intersectionDistance(intersection, pathA, pathB):
+	return max(abs(intersection.x - pathA.start.x), abs(intersection.y - pathA.start.y)) + max(abs(intersection.x - pathB.start.x), abs(intersection.y - pathB.start.y))
+
 
 def getClosestIntersection(intersections):
-	intersections.remove(Coordinate(0, 0))
+	intersections = list(filter(lambda i: i.coordinate != Coordinate(0, 0), intersections))
 
-	return min(intersections, key=lambda coord: coord.distanceFromCenter())
+	return min(intersections, key=lambda inter: inter.steps)
 
 
 run("day3_input.txt")
